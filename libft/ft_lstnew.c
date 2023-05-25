a59da24e6c3bdbed8ec43d22dbdea88cd64788c9
Masaru Ogawa <mogawa@c1r9s1.42tokyo.jp>
2023-05-25 13:23:44 +0900

    libft *.c files have been moved to libft folder and .h file moved to include so that easy to pull for projects going forward

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 18:55:26 by mogawa            #+#    #+#             */
+/*   Updated: 2023/05/24 16:48:35 by mogawa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// alloc & rtn a new node of the list
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*lst;
+
+	lst = ft_calloc(1, sizeof(t_list));
+	if (lst == NULL)
+		return (NULL);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
+}
