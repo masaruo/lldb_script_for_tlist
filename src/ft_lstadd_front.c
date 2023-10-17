fc85c6f47b13b5b4be01a0dc4ec3f0771d916d5c
masaru ogawa <masaru@gmail.com>
2023-10-17 15:43:36 +0900

    bugfix in ft_lstadd_front bugdesc: [oldhead = new], instead of [*lst = new]

--- src/ft_lstadd_front.c
+++ src/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 19:59:43 by mogawa            #+#    #+#             */
-/*   Updated: 2023/09/05 18:09:44 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/10/17 15:40:51 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = old_head;
 	if (old_head != NULL)
 		old_head->prev = new;
-	old_head = new;
+	*lst = new;
 }
